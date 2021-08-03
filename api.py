4cd444b57043539da161aa70895d07b90812cf64
Anarchy40 <ernachist@gmail.com>
2021-08-03 13:36:27 +0300

    Added nav and form

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Resource,Api
 import requests
-import 
+from flask import render_template
 
 
 app = Flask(__name__)
@@ -13,12 +13,13 @@ CORS(app)
 def home():
     a = 'dYNamite'
     b = 'Mjamaa'
-    sum = a +''+ b
+    sum = a + ''+ b
     
     
-   # r = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-
+    #r = requests.get('https://jsonplaceholder.typicode.com/todos/2')
     
-    return 
+    
+    #return  r.json()
+    return '<h1 id="sus">This isn atest </h1><br><script> document.getElementById("sus".innerHTML = "This is the test!!"</script>' + sum
 if __name__ == '__main__':
     app.run(debug=True,host = '127.0.0.1',port=666)
